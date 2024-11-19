1eff0b2a729b7b434e4595cf7b55e24ebdbd42cd
BlackHole1410 <nathan.bigot86@gmail.com>
2024-11-19 17:04:36 +0100

    Delete sortCharacters and use characters-api.js

--- src/api/characters-api.js
+++ src/api/characters-api.js
@@ -12,3 +12,23 @@ export function getCharacters() {
 export function getCharacterById(id) {
     return characters.find(character => character.id === id);
 }
+
+// Fonction pour trier les personnages
+export function getSortedCharacters(sortBy, order) {
+    return characters.slice().sort((a, b) => {
+        if (sortBy === "name") {
+            if (order === "asc") {
+                return a.name.localeCompare(b.name);
+            } else {
+                return b.name.localeCompare(a.name);
+            }
+        } else if (sortBy === "modified") {
+            if (order === "asc") {
+                return new Date(a.modified) - new Date(b.modified);
+            } else {
+                return new Date(b.modified) - new Date(a.modified);
+            }
+        }
+        return 0;
+    });
+}
